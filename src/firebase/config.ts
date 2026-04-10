df56ef2e9dd1a128765663d21d6957e981ac786e
Vivek Yadav <vivekyadav69021@gmail.com>
2026-04-10 14:33:52 +0000

    Bhai Ye New Api Key Add Kro And Abhi Jo Api Key Hai Limit End Hogya Hai

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6776438707-835e3",
   "appId": "1:708791738298:web:631338b399245b6d3b8783",
-  "apiKey": "AIzaSyDLE_q4_EiJpkE8YEB8s4mzdDoChBw-yk8",
+  "apiKey": "AIzaSyD_O0QFqs6pDv2OL_5SoZJS_VGkPIJ0QUI",
   "authDomain": "studio-6776438707-835e3.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "708791738298"
