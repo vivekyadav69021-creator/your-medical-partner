498f7cbbc97ccf5332b68fb82638b56947b70b1d
Vivek Yadav <vivekyadav69021@gmail.com>
2026-04-24 09:01:28 +0000

    Ye Me Tume Kuchh Derah hu Jo Firebase Console Se Hai Jo Iska Use Hoga to

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6776438707-835e3",
-  "appId": "1:117109899751:web:631338b399245b6d3b8783",
-  "apiKey": "AIzaSyBHHc8Ud054zJK-fkjjmS_etPIRGVCX1eE",
-  "authDomain": "studio-6776438707-835e3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "117109899751"
+  apiKey: "AIzaSyDLE_q4_EiJpkE8YEB8s4mzdDoChBw-yk8",
+  authDomain: "studio-6776438707-835e3.firebaseapp.com",
+  databaseURL: "https://studio-6776438707-835e3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-6776438707-835e3",
+  storageBucket: "studio-6776438707-835e3.firebasestorage.app",
+  messagingSenderId: "708791738298",
+  appId: "1:708791738298:web:631338b399245b6d3b8783"
 };
